93fcd07fae2bc52f16d7357408d439395b4a915b
SaipovKhasan <saipovxasan10@gmail.com>
2025-06-24 17:46:56 +0500

    git push

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:blog_id>/view', views.detail, name='detail'),
     path('about/', views.about, name='about'),
     path('<int:blog_id>/update', views.update, name='update'),
+    path('<int:blog_id>/delete', views.delete, name='delete'),
+    path('create/', views.create, name='create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
-import random
-from dataclasses import dataclass
-from django.http import HttpResponseNotFound
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 
+import blog
 from blog.forms import BlogForms
 from blog.models import Blog
-
-
-@dataclass
-class Students:
-    first_name: str
-    last_name: str
-    phone: str
-    payed: bool
-
-
-st1 = Students('Alisher', 'Eshmatov', '998 91 903 34-43', True)
-st2 = Students('Alex', 'Gorden', '998 91 903 34-43', False)
-st3 = Students('Avazbek', 'Boymuradov', '998 90 903 00-42', True)
-st4 = Students('Abror', 'Sotiboldiyev', '998 33 300 40-43', True)
-students = [st1, st2, st3, st4]
 
 
 def home(request):
@@ -56,6 +40,7 @@ def update(request, blog_id):
         form = BlogForms(request.POST, request.FILES, instance=blog)
         if form.is_valid():
             form.save()
+            messages.success(request, message=f"{blog.title} updated successfully!")
             return redirect('home')
     else:
         form = BlogForms(instance=blog)
@@ -64,3 +49,27 @@ def update(request, blog_id):
         "blog": blog
     }
     return render(request, 'blog/update.html', context=context)
+
+
+def delete(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+    blog.delete()
+    return redirect('home')
+
+
+def create(request):
+    if request.method == 'POST':
+        form = BlogForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, message=f'{blog.title} created successfully')
+            return redirect('home')
+    else:
+        messages.warning(request, message=f"We are currently in test mode!")
+        form = BlogForms()
+
+    context = {
+        "form": form,
+        "blog": blog
+    }
+    return render(request, 'blog/create_blog.html', context=context)
